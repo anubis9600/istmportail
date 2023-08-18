20bb3515b706d9f3d9495311c8c58d6e965369ad
OLIVIE RUKABO <133749488+olivierRukaboAsyst@users.noreply.github.com>
2023-08-18 17:59:29 +0200

    ajout des fichiers javascript

---
+++ public/_assets/js/main.js
@@ -0,0 +1,6 @@
+window.onload = () =>{
+    let nav = document.querySelector('div.menu-entrance')
+    // var nav = document.getElementById('menu')
+    // var nav = document.getElementsByClassName('menu-entrance')
+    nav.style.display = "none"
+}
